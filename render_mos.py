ec98efd954c5900cf21e0913714ef2535f5fe399
phong <pppppppp1202@gmail.com>
2023-12-17 19:04:14 +0700

    modify

--- render_mos.py
+++ render_mos.py
@@ -12,18 +12,33 @@ def main():
 
     html = template.render(
         page_title="MOS test",
-        form_url="http://localhost:8888",
+        form_url="https://script.google.com/macros/s/AKfycbz79ugrzOJXZ7ovv28gSEEFKxCw35nyzJJVxts889MRYDyxrurbIQkrEsj8ZKLqKzH1Ag/exec",
         form_id=1,
         questions=[
             {
                 "title": "1",
-                "audio_path": "wavs/exp1/en/1.wav",
+                "audio_path": "wavs/exp4/en/0.wav",
                 "name": "q1"
             },
             {
                 "title": "2",
-                "audio_path": "wavs/test2.wav",
+                "audio_path": "wavs/exp4/en/1.wav",
                 "name": "q2"
+            },
+            {
+                "title": "3",
+                "audio_path": "wavs/exp4/en/2.wav",
+                "name": "q3"
+            },
+            {
+                "title": "4",
+                "audio_path": "wavs/exp4/en/3.wav",
+                "name": "q4"
+            },
+            {
+                "title": "5",
+                "audio_path": "wavs/exp4/en/4.wav",
+                "name": "q5"
             },
         ]
     )
