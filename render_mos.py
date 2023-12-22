585516da734b5e96bc03438db6ab858fa67e6491
phong <pppppppp1202@gmail.com>
2023-12-22 11:00:19 +0700

    add audios

--- render_mos.py
+++ render_mos.py
@@ -9,37 +9,17 @@ def main():
     loader = FileSystemLoader(searchpath="./templates")
     env = Environment(loader=loader)
     template = env.get_template("mos.html.jinja2")
-
+    langs = {"vi": 0, "en": 1, "lw": 2}
     html = template.render(
         page_title="MOS test",
         form_url="https://script.google.com/macros/s/AKfycbz79ugrzOJXZ7ovv28gSEEFKxCw35nyzJJVxts889MRYDyxrurbIQkrEsj8ZKLqKzH1Ag/exec",
         form_id=1,
         questions=[
             {
-                "title": "1",
-                "audio_path": "wavs/exp4/en/0.wav",
-                "name": "q1"
-            },
-            {
-                "title": "2",
-                "audio_path": "wavs/exp4/en/1.wav",
-                "name": "q2"
-            },
-            {
-                "title": "3",
-                "audio_path": "wavs/exp4/en/2.wav",
-                "name": "q3"
-            },
-            {
-                "title": "4",
-                "audio_path": "wavs/exp4/en/3.wav",
-                "name": "q4"
-            },
-            {
-                "title": "5",
-                "audio_path": "wavs/exp4/en/4.wav",
-                "name": "q5"
-            },
+                "title": str(i + 1 + langs[lang] * 5 + (j-1) * 15),
+                "audio_path": f"wavs/exp{j}/{lang}/{i}.wav",
+                "name": f"q{i}"
+            } for j in range(1, 6) for lang in langs for i in range(5)
         ]
     )
     print(html)
