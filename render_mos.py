12317baf611e80f4c4dcaf6ca7de8232ec1005fd
phong <pppppppp1202@gmail.com>
2023-12-28 22:48:55 +0700

    reduce

--- render_mos.py
+++ render_mos.py
@@ -19,7 +19,7 @@ def main():
                 "title": str(i + 1 + langs[lang] * 5 + (j-1) * 15),
                 "audio_path": f"wavs/exp{j}/{lang}/{i}.wav",
                 "name": f"q{i}"
-            } for j in range(1, 6) for lang in langs for i in range(5)
+            } for j in range(1, 4) for lang in langs for i in range(5)
         ]
     )
     print(html)
